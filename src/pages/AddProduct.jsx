0e7fe0819e98622a2603564bdc699010363ac2c8
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-24 23:40:04 +0600

    update

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 const AddProduct = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full h-auto flex flex-col gap-6 items-center justify-center p-4'>
+      <h1 className='text-2xl font-semibold'>Add Latest Item</h1>
+      <div className='w-full md:w-3/4 flex flex-col items-center justify-center gap-3'>
+        <div className=' flex flex-col items-start justify-start gap-2 w-full md:w-3/5'>
+          <label htmlFor="name">Name</label>
+          <input type="text" name='name' id='name' placeholder='enter product name' className='w-full outline-none border-2 px-3 p-1 rounded-md' />
+        </div>
+        <div className=' flex flex-col items-start justify-start gap-2 w-full md:w-3/5'>
+          <label htmlFor="category">Category</label>
+          <select name="category" id="category" className='w-full outline-none border-2 px-3 p-1 rounded-md'>
+            <option value="pizza">Pizza</option>
+            <option value="burger">Burger</option>
+            <option value="drinks">Drinks</option>
+            <option value="salad">Salad</option>
+            <option value="pasta">Pasta</option>
+          </select>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
