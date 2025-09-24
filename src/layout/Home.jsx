7e1d20cc523f841696c5105f402c5aee84c899df
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-25 00:18:44 +0600

    update

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome dear</h1>
+    <div className='w-full flex flex-col items-center justify-center text-center gap-8'>
+      <h1 className='text-3xl font-semibold'>Welcome Flavor Admin Site</h1>
+      <p>Quick Links:</p>
+      <div className='w-3/4 flex flex-wrap gap-8 justify-center'>
+        <Link className='px-4 p-1 bg-orange-200 text-white rounded-lg hover:bg-orange-500 transition duration-500' to='/orders'>Order</Link>
+        <Link className='px-4 p-1 bg-orange-200 text-white rounded-lg hover:bg-orange-500 transition duration-500' to='/addproduct'>Add Product</Link>
+        <Link className='px-4 p-1 bg-orange-200 text-white rounded-lg hover:bg-orange-500 transition duration-500' to='/signin'>Sign In</Link>
+        <Link className='px-4 p-1 bg-orange-200 text-white rounded-lg hover:bg-orange-500 transition duration-500' to='/signup'>Sign Up</Link>
+        <Link className='px-4 p-1 bg-orange-200 text-white rounded-lg hover:bg-orange-500 transition duration-500' to='/updateproduct'>Update Product</Link>
+        <Link className='px-4 p-1 bg-orange-200 text-white rounded-lg hover:bg-orange-500 transition duration-500' to='/products'>Products</Link>
+      </div>
     </div>
   )
 }
--- src/user/SignIn.jsx
+++ src/user/SignIn.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 const SignIn = () => {
   return (
     <div className='w-full flex items-center justify-center'>
-      <section className='w-full md:w-1/2 h-auto flex flex-col md:flex-row items-center justify-center gap-4'>
-        <div className='w-full flex flex-col items-center justify-center p-4'>
+      <section className='w-full lg:w-1/2 h-auto flex flex-col md:flex-row items-center justify-center gap-4'>
+        <div className='w-full flex flex-col text-center items-center justify-center p-4'>
           <h1 className='text-2xl font-semibold'>Welcome to Admin Page</h1>
           <p>Sign in & run your business faster, secured and premium</p>
           <Link to='/signup' className='text-orange-500 italic'>New worker!</Link>
 
 
         </div>
-        <div className='w-full md:w-3/4 p-4 rounded-lg flex flex-col items-center justify-center gap-3 bg-gray-50'>
+        <div className='w-full p-4 rounded-lg flex flex-col items-center justify-center gap-3 bg-gray-50'>
           <div className=' flex flex-col items-start justify-start gap-2 w-full md:w-3/5'>
             <label htmlFor="username">username</label>
             <input type="text" name='username' id='username' className='w-full outline-none border-2 px-3 p-1 rounded-md' />
