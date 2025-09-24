508b7fff47115273c093d3f88e1c707982ae163f
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-24 12:30:28 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './layout/Navbar'
+import Sidebar from './layout/Sidebar'
+import Home from './layout/Home'
+import Footer from './layout/Footer'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      <Footer/>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ContextProvider } from './components/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<ContextProvider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</ContextProvider>);
