cb4c4ed3a955ef11b309ea427a4bfbd313eee379
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-27 09:27:01 +0600

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
   return (
     <nav className='w-full h-20 fixed flex flex-row items-center justify-between bg-gray-100 px-4'>
         <a href="/" className='text-2xl font-bold text-orange-500'>Flavor-Admin</a>
-        <div className='w-auto h-full md:flex hidden flex-row items-center justify-center gap-3 '>
+        <div className={`w-auto h-full md:flex hidden flex-row items-center justify-center gap-3 `} >
             <Link to='/addproduct' className='px-2 lg:px-3 h-full flex items-center justify-center border-orange-500 p-1 hover:border-b-2 font-semibold'>Add Product</Link>
             <Link to='/updateproduct' className='px-2 lg:px-3 h-full flex items-center justify-center border-orange-500 p-1 hover:border-b-2 font-semibold'>Update Product</Link>
             <Link to='/products' className='px-2 lg:px-3 h-full flex items-center justify-center border-orange-500 p-1 hover:border-b-2 font-semibold'>Product</Link>
         </div>
         <div className='w-auto h-full flex flex-row items-center justify-center gap-3 '>
-            <Link to='/orders' className='bg-orange-500 text-white px-4 p-1 font-semibold rounded-lg cursor-pointer'>Check Orders</Link>
+            <Link to='/orders' className={`bg-orange-500 text-white px-4 p-1 font-semibold rounded-lg cursor-pointer ${localStorage.getItem('admin_token')? 'block': 'hidden'}`}>Check Orders</Link>
             <p className='text-2xl text-orange-500 px-4 cursor-pointer' onClick={()=> setIsSidebar(!isSidebar)}><HiOutlineBars3CenterLeft/></p>
         </div>
     </nav>
